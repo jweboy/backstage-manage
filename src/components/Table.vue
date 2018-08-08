d9b2223b09baabdaaaf14b686d094c55f185b92c
jweboy <1259524459@qq.com>
2018-08-08 14:36:09 +0800

    Add 通用table组件基础部分

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,10 +1,30 @@
 <template>
-    <Table border />
+    <Table v-bind="options" :columns="columns" :data="data" />
 </template>
 <script lang="ts">
     import { Component, Prop, Vue } from 'vue-property-decorator';
 
+    interface ColumnItems {
+        title: string;
+        key: string;
+        render: () => void;
+    }
+    interface Options {
+        border: boolean;
+        height: number | string;
+    }
+
     @Component
-    class 
+    class AMTable extends Vue {
+        @Prop() public columns!: ColumnItems;
+        @Prop() public data !: [object];
+        @Prop({ default() {
+            return {
+                border: true,
+                height: 540,
+            };
+        }}) public options!: Options;
+    }
+    export default AMTable;
 </script>
 
--- src/views/FileStorage.vue
+++ src/views/FileStorage.vue
@@ -1,3 +1,35 @@
 <template>
-    <span>file-storage</span>
+    <div class="file-storage">
+        <AMTable :columns="tableColumns" :data="tableData" />
+    </div>
 </template>
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import { AMTable } from '@/components';
+
+    @Component({
+        components: { AMTable },
+    })
+    class FileStorage extends Vue {
+        public tableColumns = [{
+            title: '用户名',
+            key: 'login',
+        }, {
+            title: 'ID',
+            key: 'node_id',
+        }];
+        public tableData = [];
+        private mounted() {
+            this.asyncGetTableData();
+        }
+        private asyncGetTableData() {
+            fetch('https://api.github.com/users')
+                .then((result) => result.json())
+                .then((data) => {
+                    this.tableData = data;
+                });
+        }
+    }
+    export default FileStorage;
+</script>
+
