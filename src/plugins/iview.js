3ff376b99a746c9aa575a6a732cc3c9b76a25196
jweboy <1259524459@qq.com>
2018-11-11 14:05:24 +0800

    调整文件列表样式和文件上传基本约束

--- src/plugins/iview.js
+++ src/plugins/iview.js
@@ -8,15 +8,21 @@ Vue.component("Header", iView.Header);
 Vue.component("Menu", iView.Menu);
 Vue.component("MenuItem", iView.MenuItem);
 Vue.component("Icon", iView.Icon);
-// Vue.component("Upload", iView.Upload);
 Vue.component("Breadcrumb", iView.Breadcrumb);
 Vue.component("BreadcrumbItem", iView.BreadcrumbItem);
 Vue.component("Upload", iView.Upload);
 Vue.component("Row", iView.Row);
+Vue.component("Card", iView.Card);
+Vue.component("Tooltip", iView.Tooltip);
 
-// col会被识别成void元素 通过重命名解决
+// 添加别名解决和HTML标签重名的问题(col会被识别成void元素)
 // https://github.com/vuejs/eslint-plugin-vue/issues/604
 Vue.component("ICol", iView.Col);
-Vue.component("Card", iView.Card);
+
+// 将部分常用的组件挂载到Vue对象上
+Vue.prototype.$Loading = iView.LoadingBar;
+Vue.prototype.$Notice = iView.Notice;
+Vue.prototype.$Modal = iView.Modal;
+
 
 import "iview/dist/styles/iview.css";
