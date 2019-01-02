d8550c147a619520d5fc72fded8900eba688227e
jweboy <1259524459@qq.com>
2019-01-02 14:42:42 +0800

    :rocket: 更新travis配置继续测试

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 current_git_branch_log=`git log --format='[%cd - %cn]%s %h' -1`
 
-echo $current_git_branch_log
+echo "$current_git_branch_log"
 
 npm run build
 
@@ -12,7 +12,7 @@ cd dist
 
 git init
 git add -A
-git commit -m $current_git_branch_log
+git commit -m "$current_git_branch_log"
 
 git push -f https://${Github_Access_Token}@github.com/jweboy/backstage-manage.git master:gh-pages
 
