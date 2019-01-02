68dc23859838d5bc41d27604c175cbc7f9f2cd84
Travis CI User <travis@example.org>
2019-01-02 06:48:38 +0000

    [Wed Jan 2 14:46:12 2019 +0800 - jweboy]:rocket: 更新travis配置继续测试 d8550c1

---
+++ precache-manifest.d1d326ca40d72b746246b3ec39510af6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ffda37695c21bcf8fd6",
+    "url": "/css/home.fb231331.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3ffda37695c21bcf8fd6",
+    "url": "/js/home.293590d7.js"
+  },
+  {
+    "revision": "7100fae5f0cb4ad0087a",
+    "url": "/js/app.6e0e0457.js"
+  },
+  {
+    "revision": "1f7d54cb6ac384e9f37d",
+    "url": "/js/bucketList.de66fa32.js"
+  },
+  {
+    "revision": "608c8fc5d345deda7526",
+    "url": "/js/login.54f43b22.js"
+  },
+  {
+    "revision": "0030c339851350e94a11",
+    "url": "/js/chunk-vendors.26196033.js"
+  },
+  {
+    "revision": "c18eac51585aea77c68d",
+    "url": "/js/fileList.6c2e4608.js"
+  },
+  {
+    "revision": "d73df22d306f557a0d3d313d78f50f84",
+    "url": "/img/background.d73df22d.png"
+  },
+  {
+    "revision": "542a7a6d4a1aabd86814",
+    "url": "/js/about.667f84f8.js"
+  },
+  {
+    "revision": "b232328dd2daf7e4f6bee0e256d5f7d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "608c8fc5d345deda7526",
+    "url": "/css/login.09018375.css"
+  },
+  {
+    "revision": "c18eac51585aea77c68d",
+    "url": "/css/fileList.d97a3176.css"
+  },
+  {
+    "revision": "0030c339851350e94a11",
+    "url": "/css/chunk-vendors.9c8e3772.css"
+  },
+  {
+    "revision": "1f7d54cb6ac384e9f37d",
+    "url": "/css/bucketList.a8b31d71.css"
+  },
+  {
+    "revision": "7100fae5f0cb4ad0087a",
+    "url": "/css/app.78ff00c1.css"
+  }
+];
