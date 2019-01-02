900f1d26d5c2ebaa701536c0984d9e29f2e96b26
jweboy <1259524459@qq.com>
2019-01-02 14:33:52 +0800

    :rocket: 增加部署配置加以测试

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # backstage-manage
 
 `vue` 结合 `element-ui` 开发的简易版本后台管理系统，目前完成图片上传模块。
+
+## 参考
+
+- [从零开始Docker化你的Node.js应用](http://levy.work/2018-06-19-a-guide-for-chinese-frontend-developer-dockerizing-your-nodejs-app/)
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+current_git_branch_log=`git log --format='[%cd - %cn]%s %h' -1`
+
+echo $current_git_branch_log
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m $current_git_branch_log
+
+git push -f https://${Github_Access_Token}@github.com/jweboy/backstage-manage.git master:gh-pages
+
+cd -
